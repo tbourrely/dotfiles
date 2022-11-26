cedeca9749247c71c6d8d991fce6db65d11035f8
Thomas Bourrely <thomas.bourrely@protonmail.com>
2022-11-26 20:16:31 +0100

    add nvim init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,60 @@
-/Users/thomasbourrely/.config/nvim/init.lua
+vim.g.mapleader = ","
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+
+local use = require('packer').use
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+
+  use { "catppuccin/nvim", as = "catppuccin" }
+
+  use {'Shougo/deoplete.nvim', run = ':UpdateRemotePlugins'}
+  use 'deoplete-plugins/deoplete-lsp'
+
+  use 'fatih/vim-go'
+
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-surround'
+  use 'raimondi/delimitmate'
+
+  use 'airblade/vim-gitgutter'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use {
+	  'nvim-tree/nvim-tree.lua',
+	  requires = {
+		  'nvim-tree/nvim-web-devicons', -- optional, for file icons
+	  },
+	  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+end)
+
+
+require'lspconfig'.gopls.setup{}
+
+vim.opt.number = true
+
+vim.cmd.colorscheme "catppuccin"
+
+vim.g['deoplete#enable_at_startup'] = 1  -- enable deoplete at startup
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
