ead811c624b61bf5320cc62a8803b1e061366650
CDS Agent <cds@ovh.net>
2024-04-18 22:49:46 +0200

    chore(dap): attach to node process instead of running jest

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -12,21 +12,31 @@ require("dap-vscode-js").setup({
 
 for _, language in ipairs({ "typescript", "javascript" }) do
   require("dap").configurations[language] = {
+    -- {
+    --   type = "pwa-node",
+    --   request = "launch",
+    --   name = "Debug Jest Tests",
+    --   -- trace = true, -- include debugger info
+    --   runtimeExecutable = "node",
+    --   runtimeArgs = {
+    --     "./node_modules/jest/bin/jest.js",
+    --     "--runInBand",
+    --   },
+    --   rootPath = "${workspaceFolder}",
+    --   cwd = "${workspaceFolder}",
+    --   console = "integratedTerminal",
+    --   internalConsoleOptions = "neverOpen",
+    -- },
     {
       type = "pwa-node",
-      request = "launch",
-      name = "Debug Jest Tests",
-      -- trace = true, -- include debugger info
-      runtimeExecutable = "node",
-      runtimeArgs = {
-        "./node_modules/jest/bin/jest.js",
-        "--runInBand",
-      },
-      rootPath = "${workspaceFolder}",
+      request = "attach",
+      name = "Attach",
+      port = 9229,
       cwd = "${workspaceFolder}",
+      continueOnAttach = true,
       console = "integratedTerminal",
       internalConsoleOptions = "neverOpen",
-    }
+    },
   }
 end
 
