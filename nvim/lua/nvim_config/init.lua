1ec5bdb743626ac1398a0b6f2658720f6b7742e3
Thomas Bourrely <thomas.bourrely@corp.ovh.com>
2024-08-30 16:58:33 +0200

    fix(nvim): clipboard

--- nvim/lua/nvim_config/init.lua
+++ nvim/lua/nvim_config/init.lua
@@ -8,3 +8,5 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGai
   command = "if mode() != 'c' | checktime | endif",
   pattern = { "*" },
 })
+
+vim.api.nvim_set_option("clipboard", "unnamedplus")
