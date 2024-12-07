e74a359e3d368d42b8a1652420123c041f0ec73d
Thomas Bourrely <thomas.bourrely@protonmail.com>
2024-12-07 23:12:10 +0100

    feat(dap): remove ui & add dap keybindigs

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,8 +1,33 @@
 return {
 	{
 		"mfussenegger/nvim-dap",
+		keys = {
+			{ "<leader>b", function() require('dap').toggle_breakpoint() end },
+			{ "<leader>dc", function() require('dap').continue() end },
+			{ "<leader>dr", function() require('dap').repl.open() end },
+			{ "<leader>dj", function() require('dap').step_into() end },
+			{ "<leader>dk", function() require('dap').step_out() end },
+			{ "<leader>dl", function() require('dap').step_over() end },
+			{ "<leader>dh", function()
+				require('dap.ui.widgets').hover()
+			end },
+			{ "<leader>dp", function()
+				require('dap.ui.widgets').preview()
+			end },
+			{ "<leader>df", function()
+				local widgets = require('dap.ui.widgets')
+				widgets.centered_float(widgets.frames)
+			end },
+			{ "<leader>ds", function()
+				local widgets = require('dap.ui.widgets')
+				widgets.centered_float(widgets.scopes)
+			end },
+		}
+	},
+	{
+		"mxsdev/nvim-dap-vscode-js",
+		dependencies = {"mfussenegger/nvim-dap"},
 		init = function()
-			local dap = require('dap')
 			require("dap-vscode-js").setup({
 				adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, 
 			})
@@ -21,20 +46,18 @@ return {
 					},
 				}
 			end
-			vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
 		end
 	},
-	{
-		"rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
-		init = function()
-			require("dapui").setup()
-			vim.keymap.set("n", "<leader>dt", function() require("dapui").toggle({ reset = true }) end)
-		end
-	},
-	{ "mxsdev/nvim-dap-vscode-js", dependencies = {"mfussenegger/nvim-dap"} },
 	{
 		"microsoft/vscode-js-debug",
 		lazy = true,
 		build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out" 
 	},
+	{
+		"leoluz/nvim-dap-go",
+		dependencies = {"mfussenegger/nvim-dap"},
+		config = function()
+			require('dap-go').setup()
+		end
+	}
 }
