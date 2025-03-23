6ffe13ea1cdf351b4eac87105247c4fbb546e9e6
Thomas Bourrely <thomas.bourrely@protonmail.com>
2025-03-23 14:59:57 +0100

    feat(nvim): add conform.nvim

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,19 @@
+return {
+	'stevearc/conform.nvim',
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			go = { "gofmt", "goimports" },
+		},
+		-- Set default options
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500 },
+	},
+}
