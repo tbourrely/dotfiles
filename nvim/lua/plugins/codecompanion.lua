5e16c69c4b13bf3fb9646d9d0397c47796984aa4
Thomas Bourrely <thomas.bourrely@corp.ovh.com>
2025-03-29 13:22:04 +0100

    feat(nvim): add codecompanion for ai endpoints

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,70 @@
+return {
+	{
+		"olimorris/codecompanion.nvim",
+		enabled = function()
+			return os.getenv("AI_ENDPOINTS_API_KEY") ~= nil
+		end,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {
+			adapters = {
+				ovhcloud_ai_endpoint = function()
+					return require("codecompanion.adapters").extend("openai_compatible", {
+						env = {
+							url = "https://llama-3-3-70b-instruct.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat",
+							api_key = "AI_ENDPOINTS_API_KEY",
+						},
+						schema = {
+							model = {
+								default = "Meta-Llama-3_3-70B-Instruct",
+							},
+						},
+					})
+				end,
+			},
+			display = {
+				chat = {
+					window = { position = "right" },
+				},
+				action_palette = {
+					provider = "telescope",
+				},
+			},
+			strategies = {
+				chat = {
+					adapter = "ovhcloud_ai_endpoint",
+					slash_commands = {
+						["workspace"] = {
+							opts = { provider = "telescope" },
+						},
+						["symbols"] = {
+							opts = { provider = "telescope" },
+						},
+						["help"] = {
+							opts = { provider = "telescope" },
+						},
+						["buffer"] = {
+							opts = { provider = "telescope" },
+						},
+						["file"] = {
+							opts = { provider = "telescope" },
+						},
+					},
+				},
+				inline = {
+					adapter = "ovhcloud_ai_endpoint",
+				},
+			},
+			opts = {
+				log_level = "DEBUG",
+			},
+		},
+		keys = {
+			{ '<leader>ct', "<cmd>CodeCompanionChat Toggle<cr>", desc = "toggle chat" },
+			{ 'ga', "<cmd>CodeCompanionChat Add<cr>", mode = {"v"}, desc = "add code to chat" },
+			{ '<leader>ca', "<cmd>CodeCompanionActions<cr>", mode = {"n", "v"}, desc = "toggle actions" },
+		},
+	},
+}
