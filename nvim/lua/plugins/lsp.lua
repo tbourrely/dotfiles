5d487aa3067e4e1672d7352840083d4050d7ab53
Thomas Bourrely <thomas.bourrely@protonmail.com>
2025-05-08 14:01:06 +0200

    feat(neovim): replace cmp with blink + update plugins (requires vim 0.11)

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,45 +1,13 @@
 return {
-	{ "folke/lsp-colors.nvim" },
 	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v4.x',
+		"mason-org/mason.nvim",
 		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
-
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
-			{'hrsh7th/cmp-nvim-lsp-signature-help'},
-
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
+			"mason-org/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
 		},
 		config = function()
-			local lsp = require("lsp-zero")
-			lsp.on_attach(function(client, bufnr)
-				lsp.default_keymaps({buffer = bufnr})
-				vim.keymap.set('n', 'gx', function() 
-					vim.cmd [[split]]
-					vim.lsp.buf.definition()
-				end , {buffer = bufnr})
-				vim.keymap.set('n', 'gv', function() 
-					vim.cmd [[vertical botright split]]
-					vim.lsp.buf.definition()
-				end , {buffer = bufnr})
-				vim.keymap.set('n', 'ga', function() 
-					vim.lsp.buf.code_action()
-				end , {buffer = bufnr})
-			end)
-
-			require('mason').setup({})
-			require('mason-lspconfig').setup({
+			require("mason").setup()
+			require("mason-lspconfig").setup({
 				ensure_installed = {
 					'ts_ls',
 					'eslint',
@@ -53,56 +21,18 @@ return {
 					'pylsp',
 					'svelte',
 					'cssls',
+					'lua_ls'
 				},
-				handlers = {
-					function(server_name)
-						require('lspconfig')[server_name].setup({})
-					end,
-				},
+				automatic_enable = true,
 			})
-			require('luasnip.loaders.from_vscode').lazy_load()
-			local cmp = require('cmp')
-			cmp.setup({
-				formatting = lsp.cmp_format(),
-				sources = {
-					{name = 'nvim_lsp'},
-					{name = 'path'},
-					{name = 'luasnip'},
-				},
-				mapping = cmp.mapping.preset.insert(),
-				snippet = {
-					expand = function(args)
-						require('luasnip').lsp_expand(args.body)
-					end,
-				},
-			})
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*.go",
-				callback = function()
-					local params = vim.lsp.util.make_range_params()
-					params.context = {only = {"source.organizeImports"}}
-					-- buf_request_sync defaults to a 1000ms timeout. Depending on your
-					-- machine and codebase, you may want longer. Add an additional
-					-- argument after params if you find that you have to write the file
-					-- twice for changes to be saved.
-					-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-					local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-					for cid, res in pairs(result or {}) do
-						for _, r in pairs(res.result or {}) do
-							if r.edit then
-								local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-								vim.lsp.util.apply_workspace_edit(r.edit, enc)
-							end
-						end
-					end
-					vim.lsp.buf.format({async = false})
-				end
-			})
+
 			vim.diagnostic.config({
 				virtual_text = true,
 				signs = true,
 				underline = true
 			})
+
+			-- show diagnostic message on hover
 			vim.api.nvim_create_autocmd("CursorHold", {
 				buffer = bufnr,
 				callback = function()
@@ -117,6 +47,100 @@ return {
 					vim.diagnostic.open_float(nil, opts)
 				end
 			})
+
+			-- jump to definition
+			vim.keymap.set('n', 'gd', function()
+				vim.lsp.buf.definition()
+			end, { buffer = bufnr })
+
+			-- jump to definition in a split
+			vim.keymap.set('n', 'gx', function()
+				vim.cmd [[split]]
+				vim.lsp.buf.definition()
+			end, { buffer = bufnr })
+
+			-- jump to defintion in a vertical split
+			vim.keymap.set('n', 'gv', function()
+				vim.cmd [[vertical botright split]]
+				vim.lsp.buf.definition()
+			end, { buffer = bufnr })
+
+			-- trigger code actions
+			vim.keymap.set('n', 'ga', function()
+				vim.lsp.buf.code_action()
+			end, { buffer = bufnr })
+
+			-- make lua_ls aware of vim specifics
+			vim.lsp.config('lua_ls', {
+				on_init = function(client)
+					if client.workspace_folders then
+						local path = client.workspace_folders[1].name
+						if
+							path ~= vim.fn.stdpath('config')
+							and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+						then
+							return
+						end
+					end
+
+					client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+						runtime = {
+							-- Tell the language server which version of Lua you're using (most
+							-- likely LuaJIT in the case of Neovim)
+							version = 'LuaJIT',
+							-- Tell the language server how to find Lua modules same way as Neovim
+							-- (see `:h lua-module-load`)
+							path = {
+								'lua/?.lua',
+								'lua/?/init.lua',
+							},
+						},
+						-- Make the server aware of Neovim runtime files
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME
+								-- Depending on the usage, you might want to add additional paths
+								-- here.
+								-- '${3rd}/luv/library'
+								-- '${3rd}/busted/library'
+
+								-- Or pull in all of 'runtimepath'.
+								-- NOTE: this is a lot slower and will cause issues when working on
+								-- your own configuration.
+								-- See https://github.com/neovim/nvim-lspconfig/issues/3189
+								-- library = {
+								--   vim.api.nvim_get_runtime_file('', true),
+								-- }
+							}
+						},
+						diagnostics = {
+							globals = { 'bufnr' },
+						},
+					})
+				end,
+				settings = {
+					Lua = {}
+				}
+			})
 		end
-	}
+	},
+	{
+		'saghen/blink.cmp',
+		dependencies = { 'rafamadriz/friendly-snippets' },
+		version = '1.*',
+		opts = {
+			keymap = { preset = 'default' },
+			appearance = {
+				nerd_font_variant = 'mono'
+			},
+			completion = { documentation = { auto_show = true } },
+			sources = {
+				default = { 'lsp', 'path', 'snippets', 'buffer' },
+			},
+			fuzzy = { implementation = "prefer_rust_with_warning" }
+		},
+		opts_extend = { "sources.default" }
+	},
+	{ 'folke/lsp-colors.nvim' },
 }
