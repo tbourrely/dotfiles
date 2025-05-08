3301396709573d140f722df40657a72e2880d5ce
Thomas Bourrely <thomas.bourrely@protonmail.com>
2025-05-08 11:28:13 +0200

    feat(neovim/codecompanion): add ollama adapter + rework opts configuration based on environment variables

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -2,19 +2,23 @@ return {
 	{
 		"olimorris/codecompanion.nvim",
 		enabled = function()
-			return os.getenv("AI_ENDPOINTS_API_KEY") ~= nil
+			return os.getenv("CODECOMPANION_AI_ENDPOINTS_API_KEY") ~= nil or os.getenv("CODECOMPANION_OLLAMA") ~= nil
 		end,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
 		},
-		opts = {
-			adapters = {
-				ovhcloud_ai_endpoint = function()
+		opts = function()
+			active_adapter = ""
+			adapters = {}
+
+			if os.getenv("CODECOMPANION_AI_ENDPOINTS_API_KEY") ~= nil then
+				active_adapter = "ovhcloud_ai_endpoint"
+				adapters[active_adapter] = function()
 					return require("codecompanion.adapters").extend("openai_compatible", {
 						env = {
 							url = "https://llama-3-3-70b-instruct.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat",
-							api_key = "AI_ENDPOINTS_API_KEY",
+							api_key = "CODECOMPANION_AI_ENDPOINTS_API_KEY",
 						},
 						schema = {
 							model = {
@@ -22,55 +26,79 @@ return {
 							},
 						},
 					})
-				end,
-			},
-			display = {
-				chat = {
-					window = { position = "right" },
+				end
+			end
+
+			if os.getenv("CODECOMPANION_OLLAMA") ~= nil then
+				active_adapter = "ollama_codellama_7b"
+				adapters[active_adapter] = function()
+					return require("codecompanion.adapters").extend("ollama", {
+						name = adapter,
+						schema = {
+							model = {
+								default = "codellama:7b",
+							},
+							num_ctx = {
+								default = 16384,
+							},
+							num_predict = {
+								default = -1,
+							},
+						},
+					})
+				end
+			end
+
+			return {
+				adapters = adapters,
+				display = {
+					chat = {
+						window = { position = "right" },
+					},
+					action_palette = {
+						provider = "telescope",
+					},
 				},
-				action_palette = {
-					provider = "telescope",
-				},
-			},
-			strategies = {
-				chat = {
-					adapter = "ovhcloud_ai_endpoint",
-					slash_commands = {
-						["workspace"] = {
-							opts = { provider = "telescope" },
+				strategies = {
+					chat = {
+						adapter = active_adapter,
+						slash_commands = {
+							["workspace"] = {
+								opts = { provider = "telescope" },
+							},
+							["symbols"] = {
+								opts = { provider = "telescope" },
+							},
+							["help"] = {
+								opts = { provider = "telescope" },
+							},
+							["buffer"] = {
+								opts = { provider = "telescope" },
+							},
+							["file"] = {
+								opts = { provider = "telescope" },
+							},
 						},
-						["symbols"] = {
-							opts = { provider = "telescope" },
-						},
-						["help"] = {
-							opts = { provider = "telescope" },
-						},
-						["buffer"] = {
-							opts = { provider = "telescope" },
-						},
-						["file"] = {
-							opts = { provider = "telescope" },
+					},
+					inline = {
+						adapter = active_adapter,
+						keymaps = {
+							accept_change = {
+								modes = { n = "ga" },
+								description = "Accept the suggested change",
+							},
+							reject_change = {
+								modes = { n = "gu" },
+								description = "Reject the suggested change",
+							},
 						},
 					},
 				},
-				inline = {
-					adapter = "ovhcloud_ai_endpoint",
-					keymaps = {
-						accept_change = {
-							modes = { n = "ga" },
-							description = "Accept the suggested change",
-						},
-						reject_change = {
-							modes = { n = "gu" },
-							description = "Reject the suggested change",
-						},
-					},
+				opts = {
+					log_level = "DEBUG",
 				},
-			},
-			opts = {
-				log_level = "DEBUG",
-			},
-		},
+			}
+		end,
 		keys = {
 			{ '<leader>ct', "<cmd>CodeCompanionChat Toggle<cr>", desc = "toggle chat" },
 			{ 'ga', "<cmd>CodeCompanionChat Add<cr>", mode = {"v"}, desc = "add code to chat" },
