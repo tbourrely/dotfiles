44dbf783feb648fd512b1cd6f5138a96837f5347
Thomas Bourrely <thomas.bourrely@ovhcloud.com>
2025-08-12 11:52:10 +0200

    chore(telescope): center layout + smart path display

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,11 +1,24 @@
 return {
-	'nvim-telescope/telescope.nvim', version = '0.1.5',
-	dependencies = { {'nvim-lua/plenary.nvim'} },
+	'nvim-telescope/telescope.nvim',
+	version = '0.1.5',
+	dependencies = { { 'nvim-lua/plenary.nvim' } },
 	keys =
 	{
-		{'<leader>fg', "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
-		{'<leader>ff', "<cmd>Telescope find_files<cr>", desc = "Find file"},
-		{'<leader>fb', "<cmd>Telescope buffers<cr>", desc = "Find buffer"},
-		{'<leader>fm', "<cmd>Telescope marks<cr>", desc = "Find marks"},
+		{ '<leader>fg', "<cmd>Telescope live_grep<cr>",  desc = "Live grep" },
+		{ '<leader>ff', "<cmd>Telescope find_files<cr>", desc = "Find file" },
+		{ '<leader>fb', "<cmd>Telescope buffers<cr>",    desc = "Find buffer" },
+		{ '<leader>fm', "<cmd>Telescope marks<cr>",      desc = "Find marks" },
+	},
+	opts = {
+		defaults = {
+			path_display = { "smart" },
+			layout_strategy = "center",
+			layout_config = {
+				center = {
+					width = { padding = 0 },
+					height = { padding = 0 },
+				},
+			},
+		},
 	},
 }
