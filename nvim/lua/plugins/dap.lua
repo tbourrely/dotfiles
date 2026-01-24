29fb9e3a65a357828fe49bf3537e3cc1bc69252e
Thomas Bourrely <thomas.bourrely@ovhcloud.com>
2026-01-24 13:18:50 +0100

    chore(nvim): dap config

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -4,28 +4,14 @@ return {
 		keys = {
 			{ "<leader>b",  function() require('dap').toggle_breakpoint() end },
 			{ "<leader>dc", function() require('dap').continue() end },
-			{ "<leader>dr", function() require('dap').repl.open() end },
+			{ "<leader>ds", function() require('dap').disconnect() require('dap').close() end },
 			{ "<leader>dj", function() require('dap').step_into() end },
 			{ "<leader>dk", function() require('dap').step_out() end },
 			{ "<leader>dl", function() require('dap').step_over() end },
-			{ "<leader>dh", function()
-				require('dap.ui.widgets').hover()
-			end },
-			{ "<leader>dp", function()
-				require('dap.ui.widgets').preview()
-			end },
-			{ "<leader>df", function()
-				local widgets = require('dap.ui.widgets')
-				widgets.centered_float(widgets.frames)
-			end },
-			{ "<leader>ds", function()
-				local widgets = require('dap.ui.widgets')
-				widgets.centered_float(widgets.scopes)
-			end },
+			{ "<leader>dvo", function() vim.cmd('DapViewOpen') end },
+			{ "<leader>dvc", function() vim.cmd('DapViewClose') end },
 		},
 		config = function()
-			-- require('dap').setup()
-
 			--- Gets a path to a package in the Mason registry.
 			--- Prefer this to `get_package`, since the package might not always be
 			--- available yet and trigger errors.
@@ -64,39 +50,13 @@ return {
 						stopOnEntry = false,
 						protocol = "inspector",
 						cwd = "${workspaceFolder}",
-						-- continueOnAttach = true,
 						autoAttachChildProcesses = true,
 						console = "integratedTerminal",
-						-- internalConsoleOptions = "neverOpen",
 					},
 				}
 			end
 		end
 	},
-	-- {
-	-- 	"mxsdev/nvim-dap-vscode-js",
-	-- 	dependencies = { "mfussenegger/nvim-dap" },
-	-- 	init = function()
-	-- 		require("dap-vscode-js").setup({
-	-- 			adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
-	-- 		})
-	-- 		--
-	-- 		for _, language in ipairs({ "typescript", "javascript" }) do
-	-- 			require("dap").configurations[language] = {
-	-- 				{
-	-- 					type = "pwa-node",
-	-- 					request = "attach",
-	-- 					name = "Attach",
-	-- 					port = 9229,
-	-- 					cwd = "${workspaceFolder}",
-	-- 					continueOnAttach = true,
-	-- 					console = "integratedTerminal",
-	-- 					internalConsoleOptions = "neverOpen",
-	-- 				},
-	-- 			}
-	-- 		end
-	-- 	end
-	-- },
 	{
 		"microsoft/vscode-js-debug",
 		lazy = true,
@@ -110,16 +70,7 @@ return {
 		end
 	},
 	{
-		"rcarriga/nvim-dap-ui",
+		"igorlfs/nvim-dap-view",
 		dependencies = { "mfussenegger/nvim-dap" },
-		config = function()
-			require("dapui").setup()
-			local dap, dapui = require("dap"), require("dapui")
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open({ reset = true })
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-			dap.listeners.before.event_exited["dapui_config"] = dapui.close
-		end
 	}
 }
